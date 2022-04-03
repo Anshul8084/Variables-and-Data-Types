45fa7d27e28068d375e69fdf9b483686bb8be173
Anshul8084 <102891644+Anshul8084@users.noreply.github.com>
2022-04-03 10:58:48 +0530

    Add files via upload

---
+++ Var_and_datatypes2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    int b = 12;
+    float pi = 3.14;
+    char c = 'A';
+    
+    cout<<"The value of a is : "<<a<<"\nThe value of b is : "<<b ;
+    cout<<"\nThe value of pi is : "<<pi;
+    cout<<"\nThe value of c is : "<<c;
+    return 0;
+}
